c997c0b120d03f95ba03e856d14879e94a77ac87
musaprg <conie112@gmail.com>
2017-11-26 00:11:00 +0900

    Fix to send registered data as websocket data

--- app/jobs/broadcast_register_job.rb
+++ app/jobs/broadcast_register_job.rb
@@ -1,10 +1,17 @@
 class BroadcastRegisterJob < ApplicationJob
   queue_as :default
 
-  def perform(register_obj)
+  def perform(register_obj, is_delete)
     # Do something later
     # TODO: Implements This Job
-    ActionCable.server.broadcast "register_channel",
+    ActionCable.server.broadcast "register_channel", {
+        is_delete: is_delete,
+        name: register_obj.name,
+        number: register_obj.number,
+        range: register_obj.range,
+        time: register_obj.time,
+        todo: register_obj.todo
+    }
       # add_comment: register_obj.content,
       # hoge: register_obj.fuga
   end
