ae62106295776d41e33f5147f090073ec04b9a0f
musaprg <conie112@gmail.com>
2017-11-25 20:54:27 +0900

    Add TODO

--- app/jobs/broadcast_register_job.rb
+++ app/jobs/broadcast_register_job.rb
@@ -3,6 +3,7 @@ class BroadcastRegisterJob < ApplicationJob
 
   def perform(register_obj)
     # Do something later
+    # TODO: Implements This Job
     ActionCable.server.broadcast "register_channel",
       # add_comment: register_obj.content,
       # hoge: register_obj.fuga
