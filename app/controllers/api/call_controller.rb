fa46cff91a34669ee119e59f7cafae6274446bd3
Shikugawa <Shikugawa@gmail.com>
2017-11-26 12:49:18 +0900

    timezone修正

--- app/controllers/api/call_controller.rb
+++ app/controllers/api/call_controller.rb
@@ -1,6 +1,6 @@
 class Api::CallController < ApplicationController
   def registers
-    time = Time.now
+    time = Time.now.getlocal('+09:00')
     current_time = "#{time.year}-#{time.month}-#{time.day} #{time.hour}:#{time.min}:00"
     @registers = Register.where('time < ?', current_time)
     render json: @registers
